cdb979f9e13ff04252684a7eb8223bef05312c85
Gabriela <gabrielaoljacic@gmail.com>
2025-04-19 05:13:22 -0400

    add css file

--- myapp/react-frontend/src/components/MeetingEnded.js
+++ myapp/react-frontend/src/components/MeetingEnded.js
@@ -1,5 +1,22 @@
-function MeetingEnded() {
-  return <div>Meeting Ended</div>;
+import '../styles/endmeeting.css';
+
+function MeetingEnded({ onRejoin }) {
+  return (
+    <div className="meeting-ended-container">
+      <div className="meeting-ended-content">
+        <h2 className="meeting-ended-title">Meeting Has Ended</h2>
+        
+        <div className="meeting-ended-actions">
+          <button 
+            className="btn-secondary"
+            onClick={() => window.location.href = '/'}
+          >
+            Return Home
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default MeetingEnded;
